ffecf1122e54f3a9b04f24e30705c522814014dc
Abhishek3285 <a.koshta2610@gmail.com>
2026-06-18 08:22:51 +0530

    Files added

--- Assignments/A7-Task-Manager/script.js
+++ Assignments/A7-Task-Manager/script.js
@@ -1,1 +1,7 @@
-console.log('Hello dostooon')
+const task_btn = document.querySelector('#taskbtn')
+const form = document.querySelector('#form')
+console.log(task_btn);
+
+task_btn.addEventListener('click', (e) => {
+    console.log(form);
+})
